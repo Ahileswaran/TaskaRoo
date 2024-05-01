b87737152c721cc40e51c2b41fb348939e64d5a7
Ahileswaran <akhilleswaran@gmail.com>
2024-05-01 06:19:18 +0530

    Added task activity with button

---
+++ app/src/main/java/com/example/taskaroo/AddTaskActivity.java
@@ -0,0 +1,9 @@
+package com.example.taskaroo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class AddTaskActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/example/taskaroo/MainActivity.java
+++ app/src/main/java/com/example/taskaroo/MainActivity.java
@@ -1,7 +1,23 @@
 package com.example.taskaroo;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fabAddTask = findViewById(R.id.fab_add_task);
+        fabAddTask.setOnClickListener(view -> {
+            // Open a new activity window to add tasks
+            Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+            startActivity(intent);
+        });
+    }
 }
