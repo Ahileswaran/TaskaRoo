7895343d6ea8004b48d72d289342a99ea05643c3
Ahileswaran <akhilleswaran@gmail.com>
2024-05-20 03:03:46 +0530

    start Activity skipped when touch

--- app/src/main/java/com/example/taskaroo/StartActivity.java
+++ app/src/main/java/com/example/taskaroo/StartActivity.java
@@ -1,8 +1,10 @@
 package com.example.taskaroo;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import com.bumptech.glide.Glide;
 
 public class StartActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +29,29 @@ public class StartActivity extends AppCompatActivity {
         // Load the first GIF
         Glide.with(this).load(R.drawable.do_animation).into(imageView);
 
-        // Handler to switch to the second GIF after first completes
+        // Handler to switch to the second GIF after the first one completes
         new Handler().postDelayed(() -> {
             Glide.with(this).load(R.drawable.taskaroo).into(imageView);
 
-            // Handler to start MainActivity after second GIF
+            // Handler to start MainActivity after the second GIF
             new Handler().postDelayed(() -> {
                 Intent intent = new Intent(StartActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish(); // Close this activity
             }, 5000); // Time in milliseconds for the second GIF
         }, 3000); // Time in milliseconds for the first GIF
+
+        // Adding a touch listener to switch to MainActivity when the screen is touched
+        imageView.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    // Start MainActivity
+                    Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish(); // Close this activity
+                    return true;
+            }
+            return false;
+        });
     }
 }
